f28444e225bb661a81932c276ac9be3dbf1b8a48
AZARIA <allysaidi64@gmail.com>
2021-05-29 11:37:25 +0200

    finition des tous les fonctionnalités

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Navigation from './components/Navigation';
+import {Provider} from 'react-redux';
+import Store from './store/ConfigureStore';
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <Provider store={Store}>
+        <Navigation/>
+      </Provider>
+      
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
